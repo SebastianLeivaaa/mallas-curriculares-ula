e96c15faace6037de812e4b8a83d12041a6ac43a
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-08-22 22:38:35 -0400

    Ajustando responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Curricula from "@/components/curricula";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-40 py-8 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-center md:px-40 md:py-8  bg-white">
       <Curricula/>
     </main>
   );
