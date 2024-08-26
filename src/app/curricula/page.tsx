ec3714131f63356bbb48f5355f4046ebea7ccd47
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-08-26 00:18:12 -0400

    Avance

--- src/app/curricula/page.tsx
+++ src/app/curricula/page.tsx
@@ -1,6 +1,7 @@
 "use client";
+import React, { Suspense } from 'react';
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
 import { intToRoman } from '@/utils/romanUtils';
 import { convertYear } from '@/utils/yearUtils';
 
@@ -22,9 +23,8 @@ export default function Curricula() {
     const [selectedCoursePrerequisites, setSelectedCoursePrerequisites] = useState<string[]>([]);
     const [coursesWithSelectedAsPrerequisite, setCoursesWithSelectedAsPrerequisite] = useState<string[]>([]);
 
-    const searchParams = useSearchParams()
-    const career= searchParams.get('career')
-
+    const searchParams = useSearchParams();
+    const career = searchParams.get('career');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -42,57 +42,57 @@ export default function Curricula() {
         };
 
         fetchData();
-    }, []);
+    }, [career]);
 
     const handleCourseClick = (id: string) => {
         if (data) {
             setCourseSelected(id);
-    
+
             const selectedPrerequisites = data.courses[id]?.prerequisites || [];
             setSelectedCoursePrerequisites(selectedPrerequisites);
-    
+
             const selectedAsPrerequisite = Object.entries(data.courses)
                 .filter(([_, course]) => course.prerequisites.includes(id))
                 .map(([id]) => id);
             setCoursesWithSelectedAsPrerequisite(selectedAsPrerequisite);
         }
     };
-    
-    
 
     return (
-        <div className='flex min-h-screen flex-col items-center justify-center 2xl:px-60 xl:px-28 lg:px-16 px-4 md:py-8  bg-white'>
-            <section className='w-full'>
-                {years.map((yearSemesters, yearIndex) => (
-                    <div key={yearIndex} className="mb-4">
-                        <div className="bg-blue-500 p-4 m-2 border-2 border-gray-500 rounded w-full">
-                            <h1 className="text-2xl font-bold">{convertYear(yearIndex + 1)}</h1>
-                        </div>
-                        {yearSemesters.map((semester) => (
-                            <div key={`semester-${semester}`} className="flex flex-wrap w-full gap-x-1.5 gap-y-1.5 text-black m-2">
-                                <div className='p-4 rounded border-2 border-gray-500 items-center text-start bg-blue-200 w-full xl:basis-[10%]'>
-                                    Semestre {intToRoman(semester)}
-                                </div>
-                                {Object.entries(data?.courses || {})
-                                    .filter(([_, course]) => course.semester === semester)
-                                    .map(([id, course]) => (
-                                        <button
-                                            key={id}
-                                            className={`p-4 rounded border-2 border-gray-500 flex-1 text-start xl:basis-1/12 
-                                                ${courseSelected === id ? 'bg-red-300' : ''}
-                                                ${selectedCoursePrerequisites.includes(id) ? 'bg-green-300' : ''}
-                                                ${coursesWithSelectedAsPrerequisite.includes(id) ? 'bg-yellow-300' : ''}
-                                            `}
-                                            onClick={() => handleCourseClick(id)}
-                                        >
-                                            {course.name}
-                                        </button>
-                                    ))}
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className='flex min-h-screen flex-col items-center justify-center 2xl:px-60 xl:px-28 lg:px-16 px-4 md:py-8 bg-white'>
+                <section className='w-full'>
+                    {years.map((yearSemesters, yearIndex) => (
+                        <div key={yearIndex} className="mb-4">
+                            <div className="bg-blue-500 p-4 m-2 border-2 border-gray-500 rounded w-full">
+                                <h1 className="text-2xl font-bold">{convertYear(yearIndex + 1)}</h1>
                             </div>
-                        ))}
-                    </div>
-                ))}
-            </section>
-        </div>
+                            {yearSemesters.map((semester) => (
+                                <div key={`semester-${semester}`} className="flex flex-wrap w-full gap-x-1.5 gap-y-1.5 text-black m-2">
+                                    <div className='p-4 rounded border-2 border-gray-500 items-center text-start bg-blue-200 w-full xl:basis-[10%]'>
+                                        Semestre {intToRoman(semester)}
+                                    </div>
+                                    {Object.entries(data?.courses || {})
+                                        .filter(([_, course]) => course.semester === semester)
+                                        .map(([id, course]) => (
+                                            <button
+                                                key={id}
+                                                className={`p-4 rounded border-2 border-gray-500 flex-1 text-start xl:basis-1/12 
+                                                    ${courseSelected === id ? 'bg-red-300' : ''}
+                                                    ${selectedCoursePrerequisites.includes(id) ? 'bg-green-300' : ''}
+                                                    ${coursesWithSelectedAsPrerequisite.includes(id) ? 'bg-yellow-300' : ''}
+                                                `}
+                                                onClick={() => handleCourseClick(id)}
+                                            >
+                                                {course.name}
+                                            </button>
+                                        ))}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </section>
+            </div>
+        </Suspense>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,25 +15,25 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center 2xl:px-60 xl:px-28 lg:px-16 px-4 md:py-8 bg-white text-black">
       <section className="flex flex-col w-full h-full items-center justify-center gap-y-8">
         <span className="flex flex-col">
-          <h1 className="text-4xl font-bold text-center">Programas de Grado en Ingeniería</h1>
-          <h1 className="text-4xl font-bold text-center">Universidad de Los Lagos</h1>
+          <h1 className="md:text-4xl font-bold text-center text-2xl">Programas de Grado en Ingeniería</h1>
+          <h1 className="md:text-4xl font-bold text-center text-2xl">Universidad de Los Lagos</h1>
         </span>
         <div className="grid grid-cols-1 md:grid-cols-2 p-8 gap-12">
         <button onClick={() => handleButtonClick('electricalCivilEngineering')} className='flex p-4 w-full flex-row justify-start items-center gap-x-2 border-2 border-white hover:border-gray-800 hover:bg-gray-800 hover:text-white hover:rounded-md'>
             <FaBolt className='text-2xl'/>
-            <span className='font-bold text-xl'>Ingeniería Civil Eléctrica</span>
+            <span className='font-bold md:text-xl text-lg'>Ingeniería Civil Eléctrica</span>
           </button>
           <button onClick={() => handleButtonClick('civilEngineering')} className='flex p-4 w-full flex-row justify-start items-center gap-x-2 border-2 border-white hover:border-gray-800 hover:bg-gray-800 hover:text-white rounded-md'>
             <FaBuilding className='text-2xl'/>
-            <span className='font-bold text-xl'>Ingeniería Civil</span>
+            <span className='font-bold md:text-xl text-lg'>Ingeniería Civil</span>
           </button>
           <button onClick={() => handleButtonClick('computerScienceEngineering')} className='flex p-4 w-full flex-row justify-start items-center gap-x-2 border-2 border-white hover:border-gray-800 hover:bg-gray-800 hover:text-white hover:rounded-md'>
             <FaDesktop className='text-2xl'/>
-            <span className='font-bold text-xl'>Ingeniería Civil Informática</span>
+            <span className='font-bold md:text-xl text-lg'>Ingeniería Civil Informática</span>
           </button>
           <button onClick={() => handleButtonClick('industrialCivilEngineering')} className='flex p-4 w-full flex-row justify-start items-center gap-x-2 border-2 border-white hover:border-gray-800 hover:bg-gray-800 hover:text-white hover:rounded-md'>
             <FaIndustry className='text-2xl'/>
-            <span className='font-bold text-xl'>Ingeniería Civil Industrial</span>
+            <span className='font-bold md:text-xl text-lg'>Ingeniería Civil Industrial</span>
           </button>
         </div>
       </section>
