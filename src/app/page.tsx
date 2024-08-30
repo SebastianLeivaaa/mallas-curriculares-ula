5e523de89498248a86854ddb44ced8acb49c31c5
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-08-29 21:19:35 -0400

    Avance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { FaBolt, FaBuilding } from "react-icons/fa6";
 import { FaDesktop, FaIndustry } from "react-icons/fa";
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
@@ -14,9 +15,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center 2xl:px-60 xl:px-28 lg:px-16 px-2 md:py-8 bg-white text-blue-950">
       <section className="flex flex-col w-full h-full items-center justify-center gap-y-8">
+        <Image src="/logoUlagos.png" alt="Logo Universidad de Los Lagos" width={350} height={350} />
         <span className="flex flex-col ">
           <h1 className="md:text-4xl font-bold text-center text-2xl">Programas de Grado en Ingeniería</h1>
-          <h1 className="md:text-4xl font-bold text-center text-2xl">Universidad de Los Lagos</h1>
         </span>
         <div className="flex flex-col md:grid md:grid-cols-2 md:p-8 gap-12 p-2 max-md:w-[75%] max-xs:w-full justify-center items-center">
           <button onClick={() => handleButtonClick('electricalCivilEngineering')} className='flex p-4 w-full flex-row justify-center md:justify-start items-center gap-x-2 border-2 border-white hover:border-blue-800 hover:bg-blue-800 hover:text-white hover:rounded-md'>
